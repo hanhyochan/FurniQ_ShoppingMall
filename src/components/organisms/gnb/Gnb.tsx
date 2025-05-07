1ab75a0b168adf40eb3761e3bcde2521d4a32da5
hanhyochan <hcartist569@gmail.com>
2025-05-07 23:08:34 +0900

    feat: gnb 컨포넌트 작업중

--- src/components/organisms/gnb/Gnb.tsx
+++ src/components/organisms/gnb/Gnb.tsx
@@ -1,7 +1,20 @@
 import gnbStyles from './gnbStyle.module.css';
+import Heading from '@/components/atoms/text/Headings/Headings';
+import BodyText from '@/components/atoms/text/bodyText/BodyText';
 
 const Gnb = () => {
-  return <div className={gnbStyles.container}></div>;
+  return (
+    <div className={gnbStyles.container}>
+      <div className={gnbStyles.headerWrapper}>
+        <div className={gnbStyles.logoWrapper}>
+          <Heading tag="heading2_sb">FurniQ</Heading>
+        </div>
+        <BodyText tag="body2" className={gnbStyles.loginBtn}>
+          Login
+        </BodyText>
+      </div>
+    </div>
+  );
 };
 
 export default Gnb;
